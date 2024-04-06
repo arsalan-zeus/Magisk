3400402c19f1d3193dc9a92d13acd062f0f0bb0c
arsalan-zeus <prince.arsalang123@gmail.com>
2024-04-06 15:11:06 +0530

    fix init boot issue

--- scripts/util_functions.sh
+++ scripts/util_functions.sh
@@ -381,9 +381,9 @@ find_boot_image() {
   if $RECOVERYMODE; then
     BOOTIMAGE=$(find_block "recovery_ramdisk$SLOT" "recovery$SLOT" "sos")
   elif [ ! -z $SLOT ]; then
-    BOOTIMAGE=$(find_block "ramdisk$SLOT" "recovery_ramdisk$SLOT" "init_boot$SLOT" "boot$SLOT")
+    BOOTIMAGE=$(find_block "ramdisk$SLOT" "recovery_ramdisk$SLOT" "boot$SLOT")
   else
-    BOOTIMAGE=$(find_block ramdisk recovery_ramdisk kern-a android_boot kernel bootimg init_boot boot lnx boot_a)
+    BOOTIMAGE=$(find_block ramdisk recovery_ramdisk kern-a android_boot kernel bootimg boot lnx boot_a)
   fi
   if [ -z $BOOTIMAGE ]; then
     # Lets see what fstabs tells me
